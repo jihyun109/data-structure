1497a710caa9d52c2e3be0dbe2d6c3a84625e76a
jihyun109 <wlguswlgus989@gmail.com>
2024-01-13 19:02:21 +0900

    fail to find friend

--- src/Main/java/ctChap04/P13023_친구관계파악하기.java
+++ src/Main/java/ctChap04/P13023_친구관계파악하기.java
@@ -4,32 +4,56 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class P13023_친구관계파악하기 {
-	static boolean visited[];
-	static ArrayList<Integer> A[];
+	static boolean[] visited;
+	static ArrayList<Integer>[] A=new ArrayList[1000];
+	static int count = 0;
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
-		int p;
-		int q;
-		int count = 0;
-		A = new ArrayList[N + 1];
+		Scanner st = new Scanner(System.in);
+		int N = st.nextInt();
+		int M = st.nextInt();
+
+		int p = 0;
+		int q = 0;
+//		A = new ArrayList<>(N + 1);
+
 		visited = new boolean [N + 1];
 		for (int i = 1; i < N + 1; i++) {
-			A[i] = new ArrayList<Integer>();
+			A[i]=new ArrayList<Integer>();
 		}
+
 		for (int i = 0; i < M; i++) {
-			p = Integer.parseInt(st.nextToken());
-			q = Integer.parseInt(st.nextToken());
+			p = st.nextInt();
+			q = st.nextInt();
 			A[p].add(q);
 		}
+
 		for (int i = 1; i < N + 1; i++) {
-			if ()
+			if (!visited[i]) {
+				count = 0;
+				DFS(i);
+			}
+			if (count == 5) {
+				System.out.print("1");
+				break;
+			}
 		}
+		if (count < 5)
+			System.out.println("0");
 	}
 
+	static void DFS(int vIdx) {
+		if (visited[vIdx])
+			return;
+		visited[vIdx] = true;
+		count++;
+		for (Integer i : A[vIdx]) {
+			DFS(vIdx);
+			if (count == 5)
+				return;
+		}
+	}
 }
